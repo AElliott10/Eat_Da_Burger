dac8d78a7db7774bbddb0247a8413f10d65d0eba
Chingford <chingford162@outlook.com>
2019-11-27 16:36:18 -0500

    initial files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE clients
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR('255') NOT NULL,
+    devoured TINYINT NOT NULL,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id) VALUES ('Amazing Burger', true);
+INSERT INTO burgers (id) VALUES ('Marilyn Manson', false);
+INSERT INTO burgers (id) VALUES ('The Montana', true);
